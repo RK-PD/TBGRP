ecaa57d8c1301521906b5fa564460b6c9f39352c
Purva Patil <150255656+RK-PD@users.noreply.github.com>
2023-12-01 19:56:04 +0530

    PurvaPatil0911 Create Attendance Management System .java
    Task 3 - Attendance Management System in Java
    
    Develop an Attendance Management System using Java, following the MVC architecture and utilizing Maven as a build tool. MySQL will be used as the backend database, and the application will be deployed on a Tomcat server for live access. This system caters to universities, schools, and organizations for tracking student or employee attendance. 
    
    Key Features : 
    - Separate user roles: Admin, Student, Faculty. 
    - Admin privileges for adding, updating, and deleting user records. 
    - User-friendly interface for marking and viewing attendance. 
    - Database integration for storing attendance data

---
+++ PurvaPatil0911 Create Attendance Management System .java
@@ -0,0 +1,39 @@
+// Import statements
+
+public class AttendanceTrackingSystem {
+    // Main method
+    public static void main(String[] args) {
+        // Code for initializing the application
+        
+        // Code for setting up the database connection
+        
+        // Code for creating the user interface
+        
+        // Code for handling user actions and updating the database
+    }
+}
+
+public class User {
+    // Code for defining user properties and methods
+}
+
+public class Admin extends User {
+    // Code for defining admin-specific properties and methods
+}
+
+public class Student extends User {
+    // Code for defining student-specific properties and methods
+}
+
+public class Faculty extends User {
+    // Code for defining faculty-specific properties and methods
+}
+
+public class Database {
+    // Code for handling database operations
+}
+
+public class UserInterface {
+    // Code for creating the user interface and handling user interactions
+}
+
