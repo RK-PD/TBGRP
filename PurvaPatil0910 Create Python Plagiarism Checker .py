54d2494c05a5cd427bf39a54706cf03c2d37c84c
Purva Patil <150255656+RK-PD@users.noreply.github.com>
2023-12-01 20:10:37 +0530

    PurvaPatil0910 Create Python Plagiarism Checker .py
    Task 3 - Python Plagiarism Checker in Python
    
    This tool will help users identify overlaps between their work and existing online sources, ensuring content integrity. 
    
    Key Features : 
    - Text input for plagiarism checking. 
    - NLP-based analysis for content matching. 
    - Search API integration to find online sources. 
    - Detailed plagiarism report generation. 
    
    Advanced Feature (optional) : 
    - Highlight plagiarized sections in the text. 
    - Percentage similarity score.
     - Export plagiarism reports.

---
+++ PurvaPatil0910 Create Python Plagiarism Checker .py
@@ -0,0 +1,27 @@
+class PlagiarismChecker:
+    def __init__(self):
+        pass
+
+    def check_plagiarism(self, text):
+        pass
+
+    def analyze_content(self, text):
+        pass
+
+    def search_online_sources(self, text):
+        pass
+
+    def generate_report(self):
+        pass
+
+    def highlight_plagiarized_sections(self, text):
+        pass
+
+    def calculate_similarity_score(self, text1, text2):
+        pass
+
+    def export_report(self):
+        pass
+
+checker.calculate_similarity_score(text, "Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+checker.export_report()
